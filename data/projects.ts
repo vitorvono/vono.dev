8dac765604ad0af318a0f859f65184b4e8fe562f
Vitor Vono <vitorvono@gmail.com>
2023-07-04 16:51:57 -0300

    adding projects

--- data/projects.ts
+++ data/projects.ts
@@ -7,25 +7,46 @@ export type Project = {
 
 export const projects: Project[] = [
 	{
-		name: 'React Flow Smart Edge',
-		github: 'https://github.com/vitorvono/react-flow-smart-edge',
+		name: 'ePlay',
+		github: 'https://github.com/vitorvono/eplay',
 		description:
-			'Custom edge for React Flow that never intersects with other nodes.',
-		demo: 'https://vitorvono.github.io/react-flow-smart-edge/',
+			'E-comerce project created using React and using clean code fundamentals',
+		demo: 'https://eplay-peach.vercel.app/',
 	},
 	{
-		name: 'Next Template App',
-		github: 'https://github.com/vitorvono/next-template-app',
-		description: 'Next.js template with TypeScript, Chakra UI, and more.',
-		demo: '',
+		name: 'Task mannager',
+		github: 'https://github.com/vitorvono/minhas-tarefas',
+		description:
+			'',
+		demo: 'https://minhas-tarefas-two.vercel.app/',
 	},
 	{
-		name: 'CRA Template App',
-		github: 'https://github.com/vitorvono/cra-template-app',
+		name: 'Contact Book',
+		github: 'https://github.com/vitorvono/Contact-Book',
 		description:
-			'Create React App template with TypeScript, Chakra UI, and more.',
-		demo: '',
+			'',
+		demo: 'https://phone-book-react-main-t84d.vercel.app/',
 	},
+	// {
+	// 	name: 'React Flow Smart Edge',
+	// 	github: 'https://github.com/vitorvono/react-flow-smart-edge',
+	// 	description:
+	// 		'Custom edge for React Flow that never intersects with other nodes.',
+	// 	demo: 'https://vitorvono.github.io/react-flow-smart-edge/',
+	// },
+	// {
+	// 	name: 'Next Template App',
+	// 	github: 'https://github.com/vitorvono/next-template-app',
+	// 	description: 'Next.js template with TypeScript, Chakra UI, and more.',
+	// 	demo: '',
+	// },
+	// {
+	// 	name: 'CRA Template App',
+	// 	github: 'https://github.com/vitorvono/cra-template-app',
+	// 	description:
+	// 		'Create React App template with TypeScript, Chakra UI, and more.',
+	// 	demo: '',
+	// },
 	{
 		name: 'vono.dev',
 		github: 'https://github.com/vitorvono/vono.dev',
